cc7be42ed8f7d14e6beb3c804c52084d6ca5717d
KawaCassie <cassandraguillet@gmail.com>
2023-08-31 17:29:56 +0200

    Ajout description CandyList

--- candyhouse/src/datas/candyList.js
+++ candyhouse/src/datas/candyList.js
@@ -16,62 +16,71 @@ export const candyList = [
 		category: 'Sucette',
 		id: '1ed',
 		cover : sucetterouge,
-		price : 3
+		price : 3, 
+		description : 'Savourez ces délicieuses sucettes rouges, vendues par lot de trois. Leur goût fruité et leur forme ludique en font une option idéale pour satisfaire vos envies sucrées.'
 	},
 	{
 		name: 'Mega Roll (L\'unité)',
 		category: 'Chewing-gum',
 		id: '2ab',
 		cover : megaroll,
-		price : 2
+		price : 2, 
+		description : 'Le Mega Roll vous offre une expérience de chewing-gum extraordinaire. Son goût durable et sa taille généreuse en font un choix agréable pour une explosion de saveurs rafraîchissantes.'
 	},
 	{
 		name: 'Chocolat Noir',
 		category: 'Chocolat',
 		id: '3sd',
 		cover : chocolatnoir, 
-		price : 12
+		price : 12, 
+		description : 'Laissez-vous séduire par l\'élégance du chocolat noir. Fabriqué à partir de cacao de qualité supérieure, ce chocolat offre une expérience gustative intense et raffinée pour les amateurs de chocolat noir.'
 	},
 	{
 		name: 'Carambar Fruits (Par 10)',
 		category: 'Classique',
 		id: '4kk',
 		cover : carambarfruits, 
-		price : 4
+		price : 4, 
+		descritpion : 'Découvrez le plaisir nostalgique des Carambars aux fruits. Ce lot de dix vous permet de profiter de ces délices à la fois tendres et savoureux, parfaits pour les moments gourmands.'
 	},
 	{
 		name: 'Chocolat Au Lait',
 		category: 'Chocolat',
 		id: '5pl',
 		cover : chocolatlait,
-		price : 14
+		price : 14, 
+		description : 'L\'onctuosité du chocolat au lait est à portée de main. Plongez dans la douceur de ce chocolat crémeux et laissez les saveurs vous emporter dans une expérience chocolatée incomparable.'
 	},
 	{
 		name: 'Rouleau Réglisse (Par 5)',
 		category: 'Classique',
 		id: '6uo', 
 		cover : rouleaureglisse,
-		price : 3
+		price : 3, 
+		description : 'Les amateurs de réglisse seront ravis par ces rouleaux de réglisse, vendus par lot de cinq. Leur texture à la fois moelleuse et audacieusement parfumée en fait une option incontournable.'
 	},
 	{
 		name: 'Sucette Coeur (Par 2)',
 		category: 'Sucette',
 		id: '7ie', 
 		cover : sucettecoeur,
-		price : 8
+		price : 8, 
+		description : 'Offrez à votre palais une expérience romantique avec nos sucettes en forme de cœur. Parfaites pour partager ou pour vous faire plaisir, ces douceurs sucrées égayeront votre journée.'
 	},
 	{
 		name: 'Tubble Gum',
 		category: 'Chewing-gum',
 		id: '8fp',
 		cover : tubblegum, 
-		price : 6
+		price : 6, 
+		description : 'Plongez dans une explosion de bulles de saveur avec le Tubble Gum. Sa forme unique et ses arômes variés en font un compagnon idéal pour les moments ludiques et rafraîchissants.'
 	},
 	{
 		name: 'Chamallow (300g)',
 		category: 'Classique',
 		id: '9vn',
 		cover : chamallow, 
-		price : 4
+		price : 4, 
+		description : 'Les chamallows sont parfaits pour les amateurs de douceurs moelleuses. Ce sachet de 300g vous permet de profiter de ces friandises légères et sucrées, idéales pour grignoter ou accompagner un chocolat chaud.'
 	}
 ]
